6e0b0b46c109fc276bf3fedc7ec3b388b83d74e4
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-14 23:53:09 +0200

    created array with todos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 console.log('JS OK!');
 
+const toDos = [
+    {
+        text: 'Fare la spesa',
+        done: false
+    },
+    {
+        text: 'Fare il carosello',
+        done: false
+    },
+    {
+        text: 'Studiare javascript',
+        done: false
+    },
+    {
+        text: 'Ripassare HTML e CSS',
+        done: false
+    },
+    {
+        text: 'Iscrizione palestra',
+        done: false
+    },
+]
 
 const app = new Vue({
     el: '#root',
     data: {
-
+        toDos
     },
     methods: {
 
